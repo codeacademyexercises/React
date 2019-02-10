8b70007963adf338ecfe6416e6886ec6ff8cd009
Chirag Ramachandraiah <chirag_ramachandraiah@external.mckinsey.com>
2019-02-10 23:10:48 +0530

    added extra tests for ProductCategoryRow component

--- src/thinking-in-react/ProductCategoryRow/ProductCategoryRow.test.js
+++ src/thinking-in-react/ProductCategoryRow/ProductCategoryRow.test.js
@@ -16,4 +16,18 @@ describe("<ProductCategoryRow /> should ",()=>{
         const tree = renderer.create(<ProductCategoryRow category="" product={store}/>).toJSON();
         expect(tree).toMatchSnapshot();
     });
+    it("have <ProductRow/> component and should assign props correctly",()=>{
+        const wrapper = shallow(<ProductCategoryRow />);
+        expect(wrapper.find('ProductRow')).toHaveLength(1);
+        wrapper.setProps({
+            category: ['Electronics'],
+            product: [
+                {category: 'Electronics',Name: 'iPod Touch',Price: 99.99, stocked: true},
+            {category: 'Electronics',Name: 'iPhone 5',Price: 399.99, stocked: false},
+            {category: 'Electronics',Name: 'Nexus 7',Price: 199.99, stocked: true},
+            ],
+        });
+        expect(wrapper.find('ProductRow').props().category).toEqual(["Electronics"]);
+        expect(wrapper.find('ProductRow').props().items).toEqual([store[3],store[4],store[5]]);
+    });
 });
